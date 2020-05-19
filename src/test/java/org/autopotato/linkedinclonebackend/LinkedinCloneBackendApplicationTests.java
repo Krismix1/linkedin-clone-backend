b4636b6b28728196d21b246a4b56920d7924f7c6
Matej <majtan.matej@gmail.com>
2020-05-19 21:40:52 +0200

    changed to Spring application

---
+++ src/test/java/org/autopotato/linkedinclonebackend/LinkedinCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.autopotato.linkedinclonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinCloneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
