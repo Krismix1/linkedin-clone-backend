3c4321d71f23b5cfff061c964f97dba744d15fdb
Matej <majtan.matej@gmail.com>
2020-05-21 23:25:10 +0200

    Add global CORS configurer

--- src/main/java/org/autopotato/linkedinclonebackend/LinkedinCloneBackendApplication.java
+++ src/main/java/org/autopotato/linkedinclonebackend/LinkedinCloneBackendApplication.java
@@ -2,11 +2,33 @@ package org.autopotato.linkedinclonebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LinkedinCloneBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LinkedinCloneBackendApplication.class, args);
+    }
+
+    /**
+     * Configure CORS settings to all mappings, origins and headers. Allow only GET and POST requests
+     * @return WebMvcConfigurer with the necessary settings
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                    .addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST")
+                    .allowedHeaders("*");
+            }
+        };
     }
 }
